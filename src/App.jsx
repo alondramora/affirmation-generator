ebd0b028307c0b66b13af4ea98088385e3b2ddb3
alondramora <mora_ac@yahoo.com>
2023-09-14 21:46:34 -0700

    created a components folder and a button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Button from "./components/Button";
 
 function App() {
   return (
@@ -6,6 +7,11 @@ function App() {
       <header>
         <h1>Affirmation Generator</h1>
       </header>
+      <section className="affirmation-wrapper">
+        <h2>Click the button for a new affirmation</h2>
+        <Button />
+        <p className="affirmation">Affirmation will go here</p>
+      </section>
     </>
   );
 }
