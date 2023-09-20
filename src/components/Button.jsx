03410d8e86cd397070c0873340c5f7841ae7813c
alondramora <mora_ac@yahoo.com>
2023-09-19 20:42:45 -0700

    the handleClick function in the Button component now console logs a random element in the affirmations array onClick

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,9 @@ import { affirmations } from "../database/db";
 // Find a way to get the handleClick value out of here and into the p tag in
 // App.jsx
 export function handleClick() {
-  console.log(affirmations[0]);
+  const randomAffirmation =
+    affirmations[(affirmations.length * Math.random()) | 0];
+  console.log(randomAffirmation);
   // when we click the button, generate a random affirmation from the affirmations db located in database > db.js
 }
 
